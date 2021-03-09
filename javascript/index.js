b011995101bc4d7e4397ac83c4a386244fc65b76
Daniel Vegerano <71361062+floatingchalkoutline@users.noreply.github.com>
2021-03-09 13:49:32 -0800

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+document.querySelector("img.img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+document.querySelector("img.img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "<div class='fas fa-flag' style='color: red;'></div> Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! <div class='fas fa-flag' style='color: red;'></div>";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
